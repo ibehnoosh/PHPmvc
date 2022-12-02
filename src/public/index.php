877efa93d41960fc92e8b7f2e844ffbf470df434
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-03 00:57:05 +0330

    modify how resolve View

--- src/public/index.php
+++ src/public/index.php
@@ -4,15 +4,21 @@ require_once __DIR__."/../../vendor/autoload.php";
 define('STORAGE_PATH', __DIR__.'/../storage');
 define('VIEW_PATH', __DIR__.'/../views');
 
-$router= new App\Router();
-$router
-    ->get('/', [App\Controllers\HomeController::class,'index'])
-    ->get('/invoice', [App\Controllers\InvoiceController::class,'index'])
-    ->get('/invoice/create', [App\Controllers\InvoiceController::class,'create'])
-    ->post('/invoice/create', [App\Controllers\InvoiceController::class,'create']);
+try {
+    $router = new App\Router();
+    $router
+        ->get('/', [App\Controllers\HomeController::class, 'index'])
+        ->get('/invoice', [App\Controllers\InvoiceController::class, 'index'])
+        ->get('/invoice/create', [App\Controllers\InvoiceController::class, 'create'])
+        ->post('/invoice/create', [App\Controllers\InvoiceController::class, 'create']);
 
 
-echo $router->resolve(
-    $_SERVER['REQUEST_URI'],
-    strtolower($_SERVER['REQUEST_METHOD'])
-);
+    echo $router->resolve(
+        $_SERVER['REQUEST_URI'],
+        strtolower($_SERVER['REQUEST_METHOD'])
+    );
+}catch (\App\Exception\RouteNotFoundException $e) {
+    //header('HTTP/101 404 Not Found');
+    http_response_code(404);
+    echo \App\View::make('error/404');
+}
